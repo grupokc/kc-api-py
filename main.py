6f23389ccd05b396882a6f4f8b8a54143005026c
M-Casarin-KC <mauricio.casarin@grupokc.com.mx>
2024-11-22 15:55:25 -0600

    Add info of files processed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import FileResponse
+from fastapi.exceptions import HTTPException
 import pandas as pd
 import os
 from typing import List
@@ -19,17 +20,24 @@ os.makedirs(RESULT_DIRECTORY, exist_ok=True)
 @app.post("/upload/")
 async def upload_files(files: List[UploadFile] = File(...)):
     if len(files) != 6:
-        return {"error": "Se deben subir exactamente 6 archivos"}
+        # return {"error": "Se deben subir exactamente 6 archivos"}
+        raise HTTPException(status_code=400, detail="Se deben subir exactamente 6 archivos")
 
     file_paths = []
     for file in files:
+        if not file.filename.endswith(".csv"):
+            # El raise va a detener la ejecución en cuanto suceda la excepción 
+            raise HTTPException(status_code=400, detail=f"El archivo {file.filename} NO es un CSV")
         file_path = os.path.join(UPLOAD_DIRECTORY, file.filename)
         with open(file_path, "wb") as buffer:
             buffer.write(await file.read())
         file_paths.append(file_path)
+    # Obtenemos la ruta del resultado y la información del precosado 
+    result_path, info_porcessed = process_and_merge_files(file_paths)
 
-    result_path = process_and_merge_files(file_paths)
-    return FileResponse(result_path, filename="resultado_final.csv")
+    return FileResponse(result_path,
+                        headers= {"Info-Message": f"{info_porcessed}"},
+                        filename="resultado_final.csv")
 
 @app.get("/")
 async def root():
@@ -39,6 +47,8 @@ async def root():
 async def get_version():
     return {"version": API_VERSION}
 
+
 @app.get("/saludo")
 async def saludar():
     return {"message": "Saludos Fraternos"}
+
--- process_files.py
+++ process_files.py
@@ -1,21 +1,22 @@
 import pandas as pd
 import os
 
-def process_and_merge_files(file_paths):
+def process_and_merge_files(file_paths) -> tuple:
     dataframes = []
     for file_path in file_paths:
-        df = pd.read_csv(file_path)
+        df = pd.read_csv(file_path, sep= ",", encoding= "latin1")
         # Aquí puedes agregar cualquier procesamiento específico que necesites
         # Por ejemplo, podríamos añadir una columna con el nombre del archivo:
-        df['archivo_origen'] = os.path.basename(file_path)
+        df.loc[:, 'archivo_origen'] = os.path.basename(file_path) # Dentro del df se añade de que archivo se obtuvo el registro
         dataframes.append(df)
     
     # Unir todos los dataframes
     result = pd.concat(dataframes, ignore_index=True)
     
     # Guardar el resultado
-    result_path = os.path.join("results", "resultado_final.csv")
-    result.to_csv(result_path, index=False)
-    
-    return result_path
+    result_path = os.path.join("results", "resultado_final.csv") #Devuelve /results/resultado_final.csv
+    result.to_csv(result_path, index=False)  
 
+    info_processed = f"Se procesaron {len(file_paths)} archivos, con {result.shape[0]} registros"
+    
+    return result_path, info_processed
